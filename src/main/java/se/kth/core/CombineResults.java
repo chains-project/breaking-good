33929d149ab3bf4b222f6e9ec13c2f3fe198ca8b
Frank Reyes <frankrg@kth.se>
2024-02-06 16:45:12 +0100

    Adding Combined results

--- src/main/java/se/kth/core/CombineResults.java
+++ src/main/java/se/kth/core/CombineResults.java
@@ -1,7 +1,14 @@
 package se.kth.core;
 
 import se.kth.breaking_changes.ApiChange;
+import se.kth.log_Analyzer.MavenErrorLog;
+import se.kth.log_Analyzer.MavenLogAnalyzer;
+import se.kth.spoon_compare.SpoonAnalyzer;
+import se.kth.spoon_compare.SpoonResults;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @lombok.Getter
@@ -10,11 +17,18 @@ public class CombineResults {
 
     private Set<ApiChange> apiChanges;
 
+    MavenLogAnalyzer mavenLog;
+
+    String dependencyGroupID;
+
+    String project;
+
+    Set<ApiChange> breakingChanges;
+
+
     public CombineResults(Set<ApiChange> apiChanges) {
         this.apiChanges = apiChanges;
     }
-
-
 
 
     public static void main(String[] args) {
@@ -22,10 +36,28 @@ public class CombineResults {
 
     }
 
+    public Set<BreakingChange> analyze() throws IOException {
+        MavenErrorLog log = mavenLog.analyzeCompilationErrors();
+        Set<BreakingChange> change = new HashSet<>();
 
 
+        log.getErrorInfo().forEach((k, v) -> {
+            SpoonAnalyzer spoonAnalyzer = new SpoonAnalyzer(dependencyGroupID, v);
+            List<SpoonResults> results = spoonAnalyzer.applySpoon(project + k);
+            findBreakingChanges(results, change);
 
+        });
 
+        return change;
+    }
 
-
+    public void findBreakingChanges(List<SpoonResults> spoonResults, Set<BreakingChange> change) {
+        spoonResults.forEach(spoonResult -> {
+            apiChanges.forEach(apiChange -> {
+                if (apiChange.getName().equals(spoonResult.getName())) {
+                    change.add(new BreakingChange(apiChange, spoonResult.getErrorInfo()));
+                }
+            });
+        });
+    }
 }
--- src/main/java/se/kth/spoon_compare/SpoonResults.java
+++ src/main/java/se/kth/spoon_compare/SpoonResults.java
@@ -1,6 +1,7 @@
 package se.kth.spoon_compare;
 
 
+import se.kth.log_Analyzer.MavenErrorLog;
 
 @lombok.Getter
 @lombok.Setter
@@ -10,18 +11,21 @@ public class SpoonResults {
     String element;
     String line;
     String pattern;
+    MavenErrorLog.ErrorInfo errorInfo;
 
-    public SpoonResults(String name, String element, String line, String pattern) {
+    public SpoonResults(String name, String element, String line, String pattern, MavenErrorLog.ErrorInfo errorInfo) {
         this.name = name;
         this.element = element;
         this.line = line;
         this.pattern = pattern;
+        this.errorInfo = errorInfo;
     }
 
     public SpoonResults() {
     }
 
     public String toString() {
-        return "SpoonResults :   name=" + this.getName() + ", element=" + this.getElement() + ", line=" + this.getLine() + ", pattern=" + this.getPattern();
+        return "SpoonResults{" + "name='" + name + '\'' + ", element='" + element + '\'' + ", line='" + line + '\'' + ", pattern='" + pattern + '\'' + ", errorInfo=" + errorInfo.toString() + '}';
+
     }
 }

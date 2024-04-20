b228f2ab63bf8e5dae0a1efac36ddb60fd9bd71c
Frank Reyes <frankrg@kth.se>
2024-04-20 11:34:53 +0200

    Add CtInvocation parser

--- src/main/java/se/kth/spoon_compare/SpoonCtInvocation.java
+++ src/main/java/se/kth/spoon_compare/SpoonCtInvocation.java
@@ -1,6 +1,8 @@
 package se.kth.spoon_compare;
 
 import japicmp.model.JApiCompatibilityChangeType;
+import se.kth.breaking_changes.ApiChange;
+import se.kth.core.Util;
 import spoon.reflect.code.CtInvocation;
 
 @lombok.Getter
@@ -9,26 +11,78 @@ public class SpoonCtInvocation {
 
     private JApiCompatibilityChangeType compatibilityChangeType;
     private CtInvocation<?> invocation;
+    private ApiChange apiChange;
 
-    public SpoonCtInvocation(JApiCompatibilityChangeType compatibilityChangeType, CtInvocation<?> invocation) {
-        this.compatibilityChangeType = compatibilityChangeType;
+    public SpoonCtInvocation(CtInvocation<?> invocation, ApiChange apiChange) {
+        this.compatibilityChangeType = apiChange.getCompatibilityChange().getType();
         this.invocation = invocation;
+        this.apiChange = apiChange;
     }
 
 
-    public void compare() {
+    public boolean compare() {
         switch (compatibilityChangeType) {
             case METHOD_REMOVED:
-                methodRemove();
+                return methodRemove();
+
+            case INTERFACE_REMOVED:
+                interfaceRemove();
                 break;
 
+            case SUPERCLASS_REMOVED:
+                superClassRemove();
+                break;
+            case CONSTRUCTOR_REMOVED:
+                return constructorRemove();
+
+            case METHOD_NOW_ABSTRACT:
+                return methodNowAbstract();
+
+            case ANNOTATION_DEPRECATED_ADDED:
+                annotationDeprecatedAdded();
+                break;
+
+            case METHOD_RETURN_TYPE_CHANGED:
+                methodReturnTypeChanged();
+                break;
+            default:
+                return false;
+
         }
-        System.out.println("Comparing SpoonCtInvocation");
+        return false;
+    }
+
+    private void methodReturnTypeChanged() {
+    }
+
+    private void annotationDeprecatedAdded() {
+    }
+
+    private boolean methodNowAbstract() {
+        String invocationName = Util.fullyQualifiedName(invocation.getExecutable());
+        String apiReference = apiChange.getReference().getFullQualifiedName();
+        return invocationName.equals(apiReference);
+    }
+
+    private void superClassRemove() {
+    }
+
+    private boolean constructorRemove() {
+        String invocationName = Util.fullyQualifiedName(invocation.getExecutable());
+        String apiReference = apiChange.getReference().getFullQualifiedName();
+        return invocationName.equals(apiReference);
+    }
+
+    private void interfaceRemove() {
+        String invocationName = Util.fullyQualifiedName(invocation.getExecutable());
+
     }
 
 
-    public void methodRemove() {
-
+    public boolean methodRemove() {
+        String invocationName = Util.fullyQualifiedName(invocation.getExecutable());
+        String apiReference = apiChange.getReference().getFullQualifiedName();
+        return invocationName.equals(apiReference);
     }
 
 

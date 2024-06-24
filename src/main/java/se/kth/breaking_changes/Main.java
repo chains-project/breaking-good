f6527f896d438ed2695798cd7205563e48513829
Frank Reyes-Garcia <125555252+frankreyesgarcia@users.noreply.github.com>
2024-06-24 03:29:12 -0400

    Updating project (#16)

--- src/main/java/se/kth/breaking_changes/Main.java
+++ src/main/java/se/kth/breaking_changes/Main.java
@@ -1,67 +1,8 @@
 package se.kth.breaking_changes;
 
-import se.kth.japianalysis.BreakingChange;
-
-import java.nio.file.Path;
-import java.util.List;
-import java.util.Set;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        ApiMetadata apiMetadata = new ApiMetadata(Path.of("/Users/frank/Documents/Work/PHD/Explaining/breaking-good/projects/0abf7148300f40a1da0538ab060552bca4a2f1d8/jasperreports-6.18.1.jar"));
-        ApiMetadata apiMetadata1 = new ApiMetadata(Path.of("/Users/frank/Documents/Work/PHD/Explaining/breaking-good/projects/0abf7148300f40a1da0538ab060552bca4a2f1d8/jasperreports-6.19.1.jar"));
-
-
-        JApiCmpAnalyze jApiCmpAnalyze = new JApiCmpAnalyze(
-                apiMetadata,
-                apiMetadata1
-        );
-
-
-//        Set<ApiChange> apiChanges = jApiCmpAnalyze.useJApiCmp();
-
-        List<BreakingChange> apiChanges = jApiCmpAnalyze.useJApiCmp_v2();
-
-
-
-        System.out.println("Number of changes: " + apiChanges.size());
-
-//        for (ApiChange apiChange : apiChanges) {
-//            System.out.println(apiChange);
-//        }
-
-
-//        List<BreakingUpdateMetadata> list =
-//                getBreakingCommit(Path.of("/Users/frank/Documents/Work/PHD/chains-project/paper/bump/data/benchmark"));
-//
-//
-//        for (BreakingUpdateMetadata breakingUpdate : list) {
-//            boolean result = ApiMetadata.download(breakingUpdate.updatedDependency().dependencyGroupID(),
-//                    breakingUpdate.updatedDependency().dependencyArtifactID(),
-//                    breakingUpdate.updatedDependency().newVersion());
-//
-//            if (result) {
-//                System.out.println("Downloaded " + breakingUpdate.updatedDependency().dependencyArtifactID() + "-" + breakingUpdate.updatedDependency().newVersion());
-//            } else {
-//                System.out.println("Failed to download " + breakingUpdate.updatedDependency().dependencyArtifactID() + "-" + breakingUpdate.updatedDependency().newVersion());
-//            }
-//
-//             result = ApiMetadata.download(breakingUpdate.updatedDependency().dependencyGroupID(),
-//                    breakingUpdate.updatedDependency().dependencyArtifactID(),
-//                    breakingUpdate.updatedDependency().previousVersion());
-//
-//            if (result) {
-//                System.out.println("Downloaded " + breakingUpdate.updatedDependency().dependencyArtifactID() + "-" + breakingUpdate.updatedDependency().newVersion());
-//            } else {
-//                System.out.println("Failed to download " + breakingUpdate.updatedDependency().dependencyArtifactID() + "-" + breakingUpdate.updatedDependency().newVersion());
-//            }
-
-//        }
-
-
+        System.out.println("Hello World");
     }
-
-
 }
--- src/main/java/se/kth/spoon_compare/Main.java
+++ src/main/java/se/kth/spoon_compare/Main.java
@@ -16,41 +16,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-        MavenLogAnalyzer mavenLog = new MavenLogAnalyzer(new File("/Users/frank/Documents/Work/PHD/Explaining/breaking-good/projects/5cf5a482bd430d81257b4ecd85b3d4f7da911621/jakartaee-mvc-sample/5cf5a482bd430d81257b4ecd85b3d4f7da911621.log"));
-
-
-        try {
-
-            ApiMetadata apiMetadata = new ApiMetadata(Path.of("/Users/frank/Documents/Work/PHD/Explaining/breaking-good/projects/5cf5a482bd430d81257b4ecd85b3d4f7da911621/jakarta.mvc-api-1.1.0.jar"));
-            ApiMetadata apiMetadata1 = new ApiMetadata(Path.of("/Users/frank/Documents/Work/PHD/Explaining/breaking-good/projects/5cf5a482bd430d81257b4ecd85b3d4f7da911621/jakarta.mvc-api-2.0.1.jar"));
-
-            JApiCmpAnalyze jApiCmpAnalyze = new JApiCmpAnalyze(
-                    apiMetadata,
-                    apiMetadata1
-            );
-
-
-            Set<ApiChange> apiChanges = jApiCmpAnalyze.useJApiCmp();
-
-
-            MavenErrorLog log = mavenLog.analyzeCompilationErrors();
-            String project = "/Users/frank/Documents/Work/PHD/Explaining/breaking-good/projects/5cf5a482bd430d81257b4ecd85b3d4f7da911621";
-
-            log.getErrorInfo().forEach((k, v) -> {
-                System.out.println(k);
-//                v.forEach(System.out::println);
-                SpoonAnalyzer spoonAnalyzer = new SpoonAnalyzer(v, apiChanges,null);
-                List<SpoonResults> results = spoonAnalyzer.applySpoon(project + k);
-
-                System.out.println("Amount of instructions: " + results.size());
-            });
-
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+        System.out.println("Hello World");
     }
 
 }

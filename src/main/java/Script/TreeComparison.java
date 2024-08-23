164f30caedcd812b56d06818181e43ba4062727b
Frank Reyes <frankrg@kth.se>
2024-08-23 16:54:41 +0200

    Setting build

--- src/main/java/Script/TreeComparison.java
+++
@@ -1,5 +0,0 @@
-package Script;
-
-public record TreeComparison(String oldVersion, String newVersion, int majorChanges, int minorChanges, int patchChanges,
-                             int versionOnlyChanges, int scopeOnlyChanges) {
-}

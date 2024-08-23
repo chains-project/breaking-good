9ff47c9167090275d6cf0d2da7cf30f8a30bfda6
Frank Reyes <frankrg@kth.se>
2024-08-23 16:08:38 +0200

    Remove unnecessary files

--- src/main/java/se/kth/core/Main.java
+++ src/main/java/se/kth/core/Main.java
@@ -17,42 +17,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-        Path oldDependency = Path.of("/Users/frank/Downloads/Test_Failures/log4j-api-2.16.0.jar");
-        Path newDependency = Path.of("/Users/frank/Downloads/Test_Failures/log4j-api-2.18.0.jar");
-
-        ApiMetadata apiMetadata = new ApiMetadata(oldDependency);
-        ApiMetadata apiMetadata1 = new ApiMetadata(newDependency);
-
-        JApiCmpAnalyze jApiCmpAnalyze = new JApiCmpAnalyze(
-                apiMetadata,
-                apiMetadata1
-        );
-
-        Set<ApiChange> apiChanges = jApiCmpAnalyze.useJApiCmp();
-        try {
-            MavenErrorLog errorLog = new MavenLogAnalyzer(new File("/Users/frank/Downloads/Test_Failures/webapp/1eb6d9d5b2a07720a0839457cee81e066dd932f2.log")).analyzeCompilationErrors();
-
-            ApiMetadata newApiVersion = new ApiMetadata(newDependency.toFile().getName(), newDependency);
-            ApiMetadata oldApiVersion = new ApiMetadata(oldDependency.toFile().getName(), oldDependency);
-
-            CombineResults combineResults = new CombineResults(apiChanges, newApiVersion, oldApiVersion, errorLog,null);
-
-            combineResults.setDependencyGroupID("org.apache.logging.log4j");
-
-            combineResults.setProject("/Users/frank/Downloads/Test_Failures");
-
-//            Changes changes = combineResults.analyze();
-//            changes.changes().forEach(change -> {
-//                        ExplanationTemplate explanationTemplate = new CompilationErrorTemplate(changes, "change");
-//                        explanationTemplate.generateTemplate();
-//                    }
-//            );
-
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+        System.out.println("Hello World!");
     }
 }
--- src/main/java/se/kth/java_version/Main.java
+++ src/main/java/se/kth/java_version/Main.java
@@ -13,17 +13,6 @@ import static se.kth.java_version.JavaVersion.generateVersionExplanation;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-        String projectPath = "/Users/frank/Documents/Work/PHD/Explaining/breaking-good/projects/11be71ab8713fe987785e9e25e4f3e410e709ab9/camunda-platform-7-mockito";
-        String logFile = "/Users/frank/Documents/Work/PHD/Explaining/breaking-good/projects/bad55510363bde900a60f13ecc744e0c244397d4/micronaut-openapi-codegen/bad55510363bde900a60f13ecc744e0c244397d4.log";
-        ChangesBetweenVersions changes = new ChangesBetweenVersions(
-                new ApiMetadata("1.0.0", Path.of("path/to/old/Oldapi.jar")),
-                new ApiMetadata("1.0.1", Path.of("path/to/new/Newapi.jar")
-                ));
-        generateVersionExplanation(changes, projectPath, logFile);
-
-
+        System.out.println("Hello World!");
     }
-
-
 }
